091438b99bba5c2a630199a60026953c0d6fed44
Matt Secoske <matt@socialassurance.com>
2019-07-25 18:00:47 -0500

    aught -> ought

--- update_via_scrape.rb
+++ update_via_scrape.rb
@@ -4,5 +4,5 @@ require 'nokogiri'
 doc = Nokogiri(HTTP.get("http://twitter.com/greg_doucette").body.to_s)
 doc.css('//a[@href*="/greg_doucette/status/"]').map {|el| el["href"] }
 
-# TODO: go to town on each post id, skipping any that are not replies, following all reply tweets back to the Threadnaught
+# TODO: go to town on each post id, skipping any that are not replies, following all reply tweets back to the Threadnought
 # (or to their rightful end)
